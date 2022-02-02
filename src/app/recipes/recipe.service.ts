dfb4b30610d9724393d53485877c53e7f5be746d
Besal Raj <besal8953@gmail.com>
2022-02-02 22:02:29 +0530

    first commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,66 @@
+import { Recipe } from './recipe.model';
+import {Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService{
+    recipesChanged=new Subject<Recipe[]>();
+
+    // private recipes: Recipe[]=[
+    //     new Recipe(
+    //         'A Test Recipe', 
+    //         'This is simply a test Recipe', 
+    //         'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]),
+    //     new Recipe('Another Test Recipe', 
+    //     'This is simply a test Recipe', 
+    //     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+    //     [
+    //         new Ingredient('Buns',2),
+    //         new Ingredient('Meat',2)
+    //     ])
+    //   ];
+
+    private recipes: Recipe[]=[];
+
+      constructor(private slService: ShoppingListService){
+
+      }
+
+      setRecipes(recipes: Recipe[]){
+        this.recipes=recipes;
+        this.recipesChanged.next(this.recipes.slice());
+      }
+
+      getRecipes(){
+          return this.recipes.slice();
+      }
+
+      getRecipe(index: number){
+          return this.recipes[index];
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients)
+      }
+
+      addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+      }
+    
+      updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+      }
+
+      deleteRecipe(index: number){
+        this.recipes.splice(index,1);
+        this.recipesChanged.next(this.recipes.slice());
+      }
+}
